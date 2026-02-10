e824a43240c142f8096c9689ff22c469f7c670fc
roymintu4-svg <roymintu4@gmail.com>
2026-02-10 13:20:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+<title>AKRSU - ALL KOCH RAJBONGSHI STUDET'S UNION</title>
+
+<!-- Fav Icon -->
+<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+
+<!-- Google Fonts -->
+<link href="../../../fonts.googleapis.com/css2cf61.css?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap" rel="stylesheet">
+<link href="../../../fonts.googleapis.com/css22a83.css?family=Merriweather+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&amp;display=swap" rel="stylesheet">
+
+<!-- Stylesheets -->
+<link href="assets/css/font-awesome-all.css" rel="stylesheet">
+<link href="assets/css/flaticon.css" rel="stylesheet">
+<link href="assets/css/owl.css" rel="stylesheet">
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/jquery.fancybox.min.css" rel="stylesheet">
+<link href="assets/css/animate.css" rel="stylesheet">
+<link href="assets/css/nice-select.css" rel="stylesheet">
+<link href="assets/css/color.css" rel="stylesheet">
+<link href="assets/css/style.css" rel="stylesheet">
+<link href="assets/css/responsive.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css">
+
+</head>
+
+
+<!-- page wrapper -->
+<body>
+
+    <div class="boxed_wrapper">
+
+
+        <!-- preloader -->
+        <div class="loader-wrap">
+            <div class="preloader">
+                <div class="preloader-close">Preloader Close</div>
+                <div id="handle-preloader" class="handle-preloader">
+                    <div class="animation-preloader">
+                        <div class="spinner"></div>
+                        <div class="txt-loading">
+                            <span data-text-preloader="A" class="letters-loading">
+                                A
+                            </span>
+                            <span data-text-preloader="K" class="letters-loading">
+                                K
+                            </span>
+                            <span data-text-preloader="R" class="letters-loading">
+                                R
+                            </span>
+                            <span data-text-preloader="S" class="letters-loading">
+                                S
+                            </span>
+                            <span data-text-preloader="U" class="letters-loading">
+                                U
+                            </span>
+                           
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <!-- preloader end -->
+
+
+      
+
+
+        <!-- sidebar cart item -->
+        <div class="xs-sidebar-group info-group info-sidebar">
+            <div class="xs-overlay xs-bg-black"></div>
+            <div class="xs-sidebar-widget">
+                <div class="sidebar-widget-container">
+                    <div class="widget-heading">
+                        <a href="#" class="close-side-widget"><i class="fal fa-times"></i></a>
+                    </div>
+                    <div class="sidebar-textwidget">
+                        <div class="sidebar-info-contents">
+                            <div class="content-inner">
+                                <div class="logo">
+                                    <a href="index.php"><img src="assets/images/ugaklogo.png" alt="" /></a>
+                                </div>
+                                <div class="content-box">
+                                    <h4>Book Now</h4>
+                                    <form action="#" method="post" class="booking-form">
+                                        <div class="form-group">
+                                            <input type="text" name="name" placeholder="Name" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="email" name="email" placeholder="Email" required="">
+                                        </div>
+                                        <div class="form-group">
+                                            <textarea name="message" placeholder="Text"></textarea>
+                                        </div>
+                                        <div class="form-group message-btn">
+                                            <button type="submit">Send Now</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="contact-info">
+                                    <h4>Contact Info</h4>
+                                    <ul>
+                                        <li>Dhubri (K.A.C.) Assam, 783334 </li>
+                                        <li><a href="tel:+916901819833">+91 6901819833</a></li>
+                                        <li><a href="mailto:info@example.com">ugakrsu23@gmail.com</a></li>
+                                    </ul>
+                                </div>
+                                <ul class="social-box">
+                                    <li class="facebook"><a href="https://www.facebook.com/share/17t9m5fBeN/" class="fab fa-facebook-f"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END sidebar widget item -->
+
+
+        <!-- main header -->
+        <header class="main-header style-one">
+            <!-- header-top -->
+            <div class="header-top">
+                <div class="auto-container">
+                    <div class="top-inner clearfix">
+                        <div class="left-column pull-left clearfix">
+                            <div class="weathre-box"><a href="#">Dhubri (K.A.C.) Assam, 783334 </a></div>
+                            
+                        </div>
+                        <div class="right-column pull-right clearfix">
+                            <ul class="info-list">
+                                <li><i class="flaticon-phone-with-wire"></i><a href="tel:+916901819833">+91 6901819833</a></li>
+                                <li>
+  <i class="fi fi-rr-envelope"></i>
+  <a href="mailto:ugakrsu23@gmail.com">ugakrsu23@gmail.com</a>
+</li>
+                            </ul>
+                            <ul class="social-links clearfix">
+                                <li><a href="https://www.facebook.com/share/17t9m5fBeN/"><i class="fab fa-facebook-square"></i></a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- header-lower -->
+            <div class="header-lower">
+                <div class="auto-container">
+                    <div class="outer-box clearfix">
+                        <div class="logo-box pull-left">
+                            <figure class="logo"><a href="index-2.html"><img style="max-height:67px;" src="assets/images/ugaklogo.png" alt=""></a></figure>
+                        </div>
+                        <div class="menu-area clearfix pull-right">
+                            <!--Mobile Navigation Toggler-->
+                            <div class="mobile-nav-toggler">
+                                <i class="icon-bar"></i>
+                                <i class="icon-bar"></i>
+                                <i class="icon-bar"></i>
+                            </div>
+                            <nav class="main-menu navbar-expand-md navbar-light">
+                                <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                                    <ul class="navigation clearfix">
+                                        <li class="current"><a href="#">Home</a></li>
+
+                                         <li><a href="about.php">About UGAKRSU</a></li>
+                                         <li><a href="events.php">Events</a></li>
+                                        <li><a href="contact.php">Contact</a></li>   
+                                    </ul>
+                                </div>
+                            </nav>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--sticky Header-->
+            <div class="sticky-header">
+                <div class="auto-container">
+                    <div class="outer-box clearfix">
+                        <div class="logo-box pull-left">
+                            <figure class="logo"><a href="index-2.html"><img src="assets/images/ugaklogo.png" alt=""></a></figure>
+                        </div>
+                        <div class="menu-area clearfix pull-right">
+                            <nav class="main-menu clearfix">
+                                <!--Keep This Empty / Menu will come through Javascript-->
+                            </nav>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+
+        <!--Scroll to top-->
+        <button class="scroll-top scroll-to-target" data-target="html">
+            <span class="fas fa-angle-up"></span>
+        </button>
+    </div>
+
+
+    <!-- jequery plugins -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/validation.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/appear.js"></script>
+    <script src="assets/js/scrollbar.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/nav-tool.js"></script>
+
+    <!-- main-js -->
+    <script src="assets/js/script.js"></script>
+
+</body><!-- End of .page_wrapper -->
+
+
+</html>
